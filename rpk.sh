1f6b24c32acb7caf2184f1b84956ab4d3ef9006d
Dave Voutila <voutilad@gmail.com>
2023-09-25 17:56:58 -0400

    tweaks and move config into "config"

---
+++ rpk.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+. ./config
+
+docker run -it --rm \
+       --name "${PREFIX}-rpk" \
+       --network "${NET}" \
+       --entrypoint rpk \
+       "${IMAGE}" \
+       -X "brokers=${PREFIX}-1:9092,${PREFIX}-2:9092,${PREFIX}-3:9092" \
+       -X "admin.hosts=${PREFIX}-1:9644,${PREFIX}-2:9644,${PREFIX}-3:9644" \
+       "$@"
